86aa411352c9f8ae671cb09492cd1f3068f687cc
JasenABaker <JasenABaker@gmail.com>
2018-01-23 11:36:49 -0500

    Created schema file. Added User to schema

---
+++ routes/db/Schema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+mongoose.Promise = global.Promise
+
+const UserSchema = new Schema (
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            sparse: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            unique: false,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            unique: false,
+        },
+        home: {
+            type: String,
+            required: false,
+            default: 'The real world',
+            unique: false
+        },
+        numberOfTrips: {
+            type: Number,
+            required: false,
+            default: 0,
+            unique: false,
+        },
+        wordsVisited: [],
+        locationsVisited: []
+    },
+    {
+        timestamps: {}
+    }
+)
+
+module.exports = {
+    UserSchema
+}
