affab42dee8c9519f352a13875c920391d9b759e
JasenABaker <JasenABaker@gmail.com>
2018-01-27 09:05:16 -0500

    updating styling

--- client/src/components/styled-components/Icon.js
+++ client/src/components/styled-components/Icon.js
@@ -25,6 +25,11 @@ const StyledSvgWorld = StyledSvg.extend`
         fill:#A4243B;
     }
 `
+const StyledSvgPlus = StyledSvgUser.extend`
+    &:hover{
+        fill:#B18FCF;
+    } 
+`
 
 export class Home extends Component {
     render() {
@@ -54,9 +59,9 @@ export class User extends Component {
 export class Plus extends Component {
     render() {
         return (
-            <StyledSvgUser mlns="http://www.w3.org/2000/svg"  version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 42 42" style={{enableBackground:"new 0 0 42 42"}}  width="40px" height="40px">
+            <StyledSvgPlus mlns="http://www.w3.org/2000/svg"  version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 42 42" style={{enableBackground:"new 0 0 42 42"}}  width="40px" height="40px">
             <path d="M37.059,16H26V4.941C26,2.224,23.718,0,21,0s-5,2.224-5,4.941V16H4.941C2.224,16,0,18.282,0,21s2.224,5,4.941,5H16v11.059  C16,39.776,18.282,42,21,42s5-2.224,5-4.941V26h11.059C39.776,26,42,23.718,42,21S39.776,16,37.059,16z" data-original="#000000" class="active-path" fill="#00000" />
-            </StyledSvgUser>
+            </StyledSvgPlus>
         )
     }
 }
