48870396e8a607440b35e480392e043e4465cc18
JasenABaker <JasenABaker@gmail.com>
2018-01-29 10:03:41 -0500

    created locations controller added get route

---
+++ routes/LocationController.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const Worlds = require('../db/models/World')
+const router = express.Router({mergeParams: true})
+
+
+router.get('/', (req,res)=>{
+    const worldId = req.params.worldId
+
+    Worlds.findById(worldId).then((world)=>{
+        res.json(world)
+    }).catch((err)=>{
+        console.error(err)
+    })
+})
